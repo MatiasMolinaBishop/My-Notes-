6a3af25ca872903992857740474ff58227e9dbfb
Matias Molina Bishop <molibi01@gmail.com>
2022-12-29 00:15:34 +0100

    test

--- src/COMPONENTS/Note.jsx
+++ src/COMPONENTS/Note.jsx
@@ -6,6 +6,7 @@ function Note(props) {
 
     return (
         <div className="note">
+            
             <h1>{props.title}</h1>
             <p>{props.content}</p>
             <button onClick={handleClick}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="10" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
